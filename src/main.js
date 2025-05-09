87cd669487689cffd727dd1d7da8eda1d1c4fabd
Jason Breedlove <j@jasonbreedlove.com>
2025-05-09 14:31:17 -0700

    Switch to Bootstrap and improve UI styling
    Replaced Materialize with Bootstrap and Bootswatch Vapor theme for a modernized UI. Updated styling and layout in HTML, restructured classes, and adjusted JavaScript to align with the new framework. Added a local vapor.css file for theme compatibility.

--- src/main.js
+++ src/main.js
@@ -1,16 +1,23 @@
-import "materialize-css/dist/css/materialize.min.css";
-import "materialize-css/dist/js/materialize.min.js";
+
 
 const { hash } = window.location;
-console.log("HASH", atob(hash.replace('#', '')));
+const message = atob(hash.replace('#', ''));
 
-document.querySelector('form').addEventListener('submit',e => {
-    e.preventDefault()
-    document.querySelector("#message-form").classList.add('hide')
-    document.querySelector("#link-form").classList.remove('hide')
-    const input = document.querySelector('#message-input')
-    const encrypted = btoa(input.value)
-    const linkInput = document.querySelector('#link-input')
-    linkInput.value = `${window.location}#${encrypted}`
-    linkInput.select()
-})
+if (message) {
+    document.querySelector('#message-form').classList.add('d-none');
+    document.querySelector('#message-show').classList.remove('d-none');
+    document.querySelector('h1').innerHTML = message;
+}
+
+document.querySelector('form').addEventListener('submit', e => {
+    e.preventDefault();
+    document.querySelector("#message-form").classList.add('d-none');
+    document.querySelector("#link-form").classList.remove('d-none');
+
+    const input = document.querySelector('#message-input');
+    const encrypted = btoa(input.value);
+    const linkInput = document.querySelector('#link-input');
+
+    linkInput.value = `${window.location}#${encrypted}`;
+    linkInput.select();
+});
